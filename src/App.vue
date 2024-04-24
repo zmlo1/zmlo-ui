5d420b281f2b9fd062cc7dab6938e3a1a8b8af19
yujinh-33 <yujinh.333@gmail.com>
2024-04-24 16:01:00 +0800

    初始化element plus

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="text-red-400">App</div>
+  <div class="text-red-400">
+    <el-button>App</el-button>
+  </div>
 </template>
 
 <style scoped></style>
